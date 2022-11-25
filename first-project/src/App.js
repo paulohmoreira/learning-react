6b152bc0e23005ea58e009c8b63612aaa43b5a29
paulohmoreira <phmoreira37@gmail.com>
2022-11-25 11:18:40 -0300

    entendendo components

--- first-project/src/App.js
+++ first-project/src/App.js
@@ -1,10 +1,10 @@
-
+import Button from "./components/Button";
 
 function App() {
   return (
     <div className="App">
       <h1>Olá, mundo!</h1>
-     
+      <Button title="Entrar"/>
     </div>
   );
 }
